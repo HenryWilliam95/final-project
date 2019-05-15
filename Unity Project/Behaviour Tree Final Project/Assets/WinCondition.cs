8a5162e43a8f6bb8743ce52d0a02005457493c02
Henry Crofts <hwcrofts@gmail.com>
2019-05-15 23:46:06 +0100

    Updated systems

---
+++ Unity Project/Behaviour Tree Final Project/Assets/WinCondition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            Debug.Log("You win");
+        }
+    }
+}
